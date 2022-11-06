7c509d6b4ff87f04bd89b837cd19bb51e62f1bb5
sarai gonzalez <saraiglezyglez@gmail.com>
2022-11-06 17:07:40 -0600

    the formTemplate for the SignUpForm.js using material ui has been created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
+import SignUp from './components/SignUpForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hello there #FutureShaper...</h3>
-        <p>
-          This is a simple React app that will be used to asses your skills.
-        </p>
-        <p>
-          Please follow the instructions in the README.md file to get started.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and show me what you got!
-        </p>
-      </header>
+        <SignUp />
     </div>
   );
 }
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/saraigyg/SaraiGonzalezSoftEng1">
+        Honeywell-Assesment
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "#e70e02" }}></Avatar>
+          <Typography component="h1" variant="h5" color={"#e70e02"} fontFamily={"Honeywell Sans, Helvetica, Arial, sans-serif"} 
+          fontWeight ={800}>
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                required
+                fullWidth
+                id="userName"
+                label="Username"
+                name="userName"
+                autoComplete="userName"
+                autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="passwordValidation"
+                  label="Password Validation"
+                  type="password"
+                  id="passwordValidation"
+                  autoComplete="password-validation"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive Honeywell news, inspiration and updates via email."
+                  style={{color: "#e70e02", fontFamily: "Honeywell Sans, Helvetica, Arial, sans-serif"}}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }} 
+              style={{ background: "#e70e02", fontWeight: 800, fontFamily: "Honeywell Sans, Helvetica, Arial, sans-serif"}}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
